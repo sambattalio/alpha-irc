39762c240638b7b144433e20e5caa46517d105d1
sambattalio <sambattalio99@gmail.com>
2019-07-31 22:38:55 -0400

    Parse incomming (minus tags) and return PONG

--- client/client.go
+++ client/client.go
@@ -2,6 +2,8 @@ package client
 
 import (
 	"fmt"
+	"strings"
+	"errors"
 	"net"
 	"bufio"
 )
@@ -12,10 +14,14 @@ type User struct {
 
 type Client struct {
 	conn net.Conn
-	err chan string
-	write chan string
 }
 
+type Message struct {
+	Tags       []string
+	Source     string
+	Command    string
+	Parameters []string
+}
 
 func Connect(c *Client, u *User) error {
 	fmt.Printf("Creating connection to %v\n", u.Server)
@@ -30,7 +36,7 @@ func Connect(c *Client, u *User) error {
 	fmt.Println("Successfully connected!")
 
 	fmt.Println("Creating readmessage goroutine")
-	go readMessage(c)
+	go readLoop(c)
 
 	fmt.Fprintf(c.conn, "NICK %v\r\n", u.Nick)
 	fmt.Fprintf(c.conn, "USER %v - * :%v\r\n", u.User, u.Name)
@@ -39,7 +45,7 @@ func Connect(c *Client, u *User) error {
 	return nil
 }
 
-func readMessage(c *Client) {
+func readLoop(c *Client) {
 	reader := bufio.NewReader(c.conn)
 
 	// loop while connected
@@ -49,6 +55,72 @@ func readMessage(c *Client) {
 			fmt.Println(err)
 			break
 		}
-		fmt.Println(msg)
+
+		parsed, err := parseMessage(msg);
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		fmt.Printf("Tags: %q\n", parsed.Tags)
+		fmt.Printf("Source: %v\n", parsed.Source)
+		fmt.Printf("command: %v\n", parsed.Command)
+		fmt.Printf("params: %q\n", parsed.Parameters)
+
+		fmt.Println("Handling command")
+
+		if (parsed.Command == "PING") {
+			fmt.Println("Sending pong")
+			fmt.Fprintf(c.conn, "PONG %v\r\n", parsed.Parameters[0])
+		}
+
 	}
+}
+
+func parseMessage(msg string) (*Message, error) {
+	if (len(msg) == 0) {
+		return &Message{}, errors.New("String Length 0!")
+	}
+
+	split := strings.Fields(msg)
+
+	parsed := &Message{}
+
+	if (strings.HasPrefix(split[0], "@")) {
+		tags, err := parseTags(split[0])
+		if err != nil {
+			return &Message{}, err
+		}
+		parsed.Tags = tags
+		split = split[1:]
+	}
+
+	if (strings.HasPrefix(split[0], ":")) {
+		parsed.Source = split[0][1:]
+		split = split[1:]
+	}
+
+	parsed.Command = split[0]
+	split = split[1:]
+
+	for i, item := range split {
+		if (strings.HasPrefix(item, ":")) {
+			parsed.Parameters = append(parsed.Parameters,
+				            strings.Join(split[i:], " ")[1:])
+			break
+		}
+		parsed.Parameters = append(parsed.Parameters, item)
+	}
+
+	return parsed, nil
+}
+
+func parseTags(s string) ([]string, error) {
+	// TODO
+	return make([]string, 0), nil
+}
+
+func handle_msg(c *Client, msg string) error {
+	fmt.Println(strings.Fields(msg))
+	return nil
 }
